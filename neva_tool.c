a6e388eb1542375bf845a1c8e3485f5acab19a5c
Ivan Vatlin <16368461+jeffangelion@users.noreply.github.com>
2021-01-13 13:08:26 +0300

    version 0.2.1
    + replaced Shift-JIS -> UTF-8 hack with iconv
    + moved (mostly) all variables to head of functions
    + now all errors makes program exit with code 1 & print message
    + removed debug output blindly left in 0.2.0 release

--- neva_tool.c
+++ neva_tool.c
@@ -15,9 +15,11 @@
  */
 
 #include <stdio.h> // printf(); fopen(); fseek(); ftell(); fread(); fclose()
+#include <stdlib.h> // exit()
 #include <math.h> // pow()
 #include <string.h> // memcpy()
 #include <stdbool.h> // bool, true, false
+#include <iconv.h> // Shift-JIS <-> UTF-8
 
 // Constants
 const char bpk0_header[4] = "BPK0";
@@ -51,43 +53,56 @@ int hex_to_dec_short (const char input[])
 	return temp;
 }
 
+int iconv_easy (char* to_code, char* from_code, char* to_buffer, size_t to_size, char *data_for_conversion)
+{
+	size_t from_size, k;
+	char* from_buffer = data_for_conversion; // Pointer for iconv() finction
+	iconv_t conversion_descriptor;
+
+	from_size = strlen (data_for_conversion);
+	conversion_descriptor = iconv_open (to_code, from_code);
+	if (conversion_descriptor == (iconv_t)(-1))
+	{
+		printf ("iconv_open() error\n");
+		exit (1);
+	}
+	k = iconv (conversion_descriptor, &from_buffer, &from_size, &to_buffer, &to_size);
+	iconv_close (conversion_descriptor);
+
+	return 0;
+}
+
 // Print file entries in folder by its address (+ name, address, and number of files in folder)
 void file_print (char folder_name[], int address, int number_of_files, FILE *file)
 {
-	if (address != 0 && number_of_files != 0)
+	char temp_file_entry[temp_file_entry_size], file_entry_name_temp[file_entry_name_size], file_entry_name[file_entry_name_size];
+	int number_of_entries, file_entry_name_offset, file_entry_full_size, file_entry_size, file_entry_address = 0;
+	bool file_entry_is_compressed;
+
+	if (number_of_files != 0)
 	{
 		fseek (file, address, SEEK_SET);
 		char get_size[4];
 		fread (get_size, 4, 1, file);
 		fseek (file, -4, SEEK_CUR); // Return file cursor to starting position
-		int number_of_entries = hex_to_dec_short (get_size) / temp_file_entry_size;
+		number_of_entries = hex_to_dec_short (get_size) / temp_file_entry_size;
 		for (int i = 0; i < number_of_entries; i++)
 		{
+			// Sanitize array before using with iconv
+			memset (&file_entry_name, 0, file_entry_name_size);
+
 			fseek (file, address + temp_file_entry_size * i, SEEK_SET);
-			char temp_file_entry[temp_file_entry_size];
 			fread (temp_file_entry, temp_file_entry_size, 1, file);
-			int file_entry_name_offset = hex_to_dec (temp_file_entry, 4, 3);
-			int file_entry_full_size = hex_to_dec (temp_file_entry, 2, 5);
-			int file_entry_size = hex_to_dec (temp_file_entry, 2, 9);
-			bool file_entry_is_compressed = file_entry_size > file_entry_full_size ? true : false;
-			int file_entry_address = hex_to_dec (temp_file_entry, 4, 15);
-			char file_entry_name[file_entry_name_size];
+
+			file_entry_name_offset = hex_to_dec (temp_file_entry, 4, 3);
+			file_entry_full_size = hex_to_dec (temp_file_entry, 2, 5);
+			file_entry_size = hex_to_dec (temp_file_entry, 2, 9);
+			file_entry_is_compressed = file_entry_size > file_entry_full_size ? true : false;
+			file_entry_address = hex_to_dec (temp_file_entry, 4, 15);
+
 			fseek (file, address + temp_file_entry_size * i + file_entry_name_offset, SEEK_SET);
-			fread (file_entry_name, file_entry_name_size, 1, file);
-			for (int j = 0; j < strlen (file_entry_name); j++)
-			{
-				// Shift-JIS -> UTF-8 fullwidth latin small letter hack
-				if (memcmp (file_entry_name + j, "\x82", 1) == 0)
-				{
-					// 0x828C -> 0xEFBD8C (Shift-JIS -> UTF-8)
-					file_entry_name[j] = 0xEF;
-					for (int k = 12; k > j; k--) // Total length should be 13 bytes
-					{
-						file_entry_name[k] = file_entry_name[k - 1];
-					}
-					file_entry_name[j + 1] = 0xBD;
-				}
-			}
+			fread (file_entry_name_temp, file_entry_name_size, 1, file);
+			iconv_easy ("UTF-8", "SHIFT-JIS", file_entry_name, file_entry_name_size, file_entry_name_temp);
 			printf ("%s,0x%x,%d,%s,%d,%d,0x%x\r\n", folder_name, address, number_of_files, file_entry_name, file_entry_is_compressed, file_entry_size, file_entry_address);
 		}
 	}
@@ -97,18 +112,13 @@ void file_print (char folder_name[], int address, int number_of_files, FILE *fil
 	}
 }
 
-// Exit codes:
-// 1 - no file
-// 2 - can't read file
-// 3 - wrong file
-
 int main (int argc, const char *argv[])
 {
 	if (argc == 2)
 	{
 		FILE *file;
 		int file_length = 0;
-		int scrolled_length = 0; // how much bytes was read from folder entries table
+		int scrolled_length = 0; // Counter of bytes read from folder entries table
 		int folder_table_address = 0;
 
 		file = fopen (argv[1], "rb");
@@ -126,9 +136,8 @@ int main (int argc, const char *argv[])
 			fseek (file, 0, SEEK_SET);
 			if (memcmp (test_header, bpk0_header, header_size) != 0)
 			{
-				printf ("%s %s\n", test_header, bpk0_header);
 				printf ("Bad file signature\n");
-				return 3;
+				exit (1);
 			}
 
 			fseek (file, 8, SEEK_SET);
@@ -140,13 +149,16 @@ int main (int argc, const char *argv[])
 			while (folder_table_address + scrolled_length < file_length)
 			{
 				fseek (file, folder_table_address + scrolled_length, SEEK_SET);
+
 				fseek (file, 4, SEEK_CUR);
 				char folder_entry_size[2];
 				fread (folder_entry_size, 2, 1, file);
 				int folder_entry_size_int = hex_to_dec_short (folder_entry_size);
 				fseek (file, -6, SEEK_CUR); // Return file cursor to starting position
+
 				char temp_folder_entry[folder_entry_size_int];
 				fread (temp_folder_entry, folder_entry_size_int, 1, file);
+
 				int folder_entry_number_of_files = hex_to_dec (temp_folder_entry, 2, 7);
 				int folder_entry_address = hex_to_dec (temp_folder_entry, 4, 11);
 				char folder_entry_name[folder_entry_size_int - folder_entry_name_offset];
@@ -160,13 +172,13 @@ int main (int argc, const char *argv[])
 		}
 		else
 		{
-			return 2;
+			printf ("Can't read file\n");
+			exit (1);
 		}
 	}
 	else
 	{
 		printf ("Usage: %s <filename>\n", argv[0]);
-		return 1;
 	}
-	return 0;
+	exit (0);
 }
